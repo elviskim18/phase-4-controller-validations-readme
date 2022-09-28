5a027964a465566711e2566e4cc32276fc07f293
Kimani <elvis.kimani@student.moringaschool.com>
2022-09-28 14:45:22 +0300

    commit tree fe8997c000145b92f32a0e319094958f4e0b06c8

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,10 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+
+
   # GET /birds
   def index
     birds = Bird.all
@@ -11,6 +15,7 @@ class BirdsController < ApplicationController
   def create
     bird = Bird.create(bird_params)
     render json: bird, status: :created
+  
   end
 
   # GET /birds/:id
@@ -24,6 +29,7 @@ class BirdsController < ApplicationController
     bird = find_bird
     bird.update(bird_params)
     render json: bird
+
   end
 
   # DELETE /birds/:id
@@ -41,6 +47,10 @@ class BirdsController < ApplicationController
 
   def bird_params
     params.permit(:name, :species, :likes)
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   def render_not_found_response
